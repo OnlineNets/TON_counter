bddc01b6291e5cdd56bb52426ab136bfa9b5b8b3
OnlineNets <nets01929@gmail.com>
2024-07-21 20:34:03 -0300

    fix node_module issue

--- scripts/deployCounter.ts
+++ scripts/deployCounter.ts
@@ -1,6 +1,6 @@
 import { toNano } from '@ton/core';
 import { Counter } from '../wrappers/Counter';
-import { compile, NetworkProvider } from '@ton/blueprint';
+import { compile, NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
     const counter = provider.open(Counter.createFromConfig({}, await compile('Counter')));
